8ff32dbaea69b210048645c85baf7649e0dcda3a
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2016-09-02 21:33:49 +0300

    Add error stack module

---
+++ include/tlog/errs.h
@@ -0,0 +1,143 @@
+/**
+ * @file
+ * @brief Error stack.
+ *
+ * An error stack is used to return messages accumulated during error return
+ * through one or more function stack frames, so the caller can present the
+ * user with detailed diagnostics, down to the lowest level, as opposed to
+ * just the topmost return code.
+ *
+ * An empty error stack is just a NULL pointer, so the sucessfull execution
+ * doesn't cost anything. Errors are added to the stack using tlog_err_push*
+ * functions.
+ */
+/*
+ *
+ * Copyright (C) 2016 Red Hat
+ *
+ * This file is part of tlog.
+ *
+ * Tlog is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tlog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tlog; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _TLOG_ERRS_H
+#define _TLOG_ERRS_H
+
+#include <stdio.h>
+#include <tlog/grc.h>
+#include <tlog/rc.h>
+
+/** An error stack item (an error) */
+struct tlog_errs {
+    struct tlog_errs   *next;   /**< Next (lower-level) error */
+    char               *msg;    /**< This error's message,
+                                     dynamically allocated */
+};
+
+/** An empty error stack */
+#define TLOG_ERRS_EMPTY    NULL
+
+/**
+ * A single-item stack used in place of a stack which failed to extend due to
+ * an allocation error. It is never destroyed by tlog_errs_destroy.
+ */
+extern const struct tlog_errs tlog_errs_nomem;
+
+/**
+ * Destroy an error stack.
+ *
+ * @param errs  Location of the error stack to destroy, cannot be NULL.
+ *              Location will be written NULL (empty stack) after destruction.
+ */
+extern void tlog_errs_destroy(struct tlog_errs **perrs);
+
+/**
+ * Push a stack to another stack.
+ *
+ * @param perrs Location of the stack to push to. The location is not modified
+ *              if the pushed stack is empty (NULL). Destroyed and replaced
+ *              with the pushed stack, if the latter ends with
+ *              tlog_errs_nomem. If NULL, then the pushed stack is destroyed.
+ * @param errs  The stack to push.
+ */
+extern void tlog_errs_push(struct tlog_errs **perrs,
+                           struct tlog_errs *errs);
+
+/**
+ * Push an error to a stack, with message as a constant string.
+ *
+ * @param perrs Location of the stack to push the error to.
+ *              If memory allocation fails, location is replaced with
+ *              tlog_errs_nomem and the original stack is freed.
+ *              If NULL, then nothing is done.
+ * @param str   The message string to copy and put into the pushed error.
+ *              Cannot be NULL.
+ */
+extern void tlog_errs_pushs(struct tlog_errs **perrs, const char *msg);
+
+/**
+ * Push an error to a stack, with message being a global return code
+ * description.
+ *
+ * @param perrs Location of the stack to push the error to.
+ *              If memory allocation fails, location is replaced with
+ *              tlog_errs_nomem and the original stack is freed.
+ *              If NULL, then nothing is done.
+ * @param grc   The global return code, which description will be pushed to
+ *              the stack.
+ */
+extern void tlog_errs_pushc(struct tlog_errs **perrs, tlog_grc grc);
+
+/**
+ * Push an error to an error stack, with a sprintf-formatted message with
+ * arguments from a va_list.
+ *
+ * @param perrs Location of the stack to push the error to.
+ *              If memory allocation fails, location is replaced with
+ *              tlog_errs_nomem and the original stack is freed.
+ *              If NULL, then nothing is done.
+ * @param fmt   The message sprintf(3) format string. Cannot be NULL.
+ * @param ap    The message format arguments.
+ */
+extern void tlog_errs_pushvf(struct tlog_errs **perrs,
+                             const char *fmt, va_list ap);
+
+/**
+ * Push an error to an error stack, with a sprintf-formatted message.
+ *
+ * @param perrs Location of the stack to push the error to.
+ *              If memory allocation fails, location is replaced with
+ *              tlog_errs_nomem and the original stack is freed.
+ *              If NULL, then nothing is done.
+ * @param fmt   The message sprintf(3) format string. Cannot be NULL.
+ * @param ...   The message format arguments.
+ */
+extern void tlog_errs_pushf(struct tlog_errs **perrs, const char *fmt, ...)
+                           __attribute__((format (printf, 2, 3)));
+
+/**
+ * Print all errors from a stack to an stdio stream, from first-pushed
+ * to last-pushed. Each error's message is appended with a newline.
+ *
+ * @param stream    The FILE stream to print the stack to.
+ *                  Cannot be NULL.
+ * @param errs      The stack to print. Can be NULL for empty stack.
+ *
+ * @return Number of characters printed, or a negative value on error, with
+ *         errno set.
+ */
+extern int tlog_errs_print(FILE *stream, const struct tlog_errs *errs);
+
+#endif /* _TLOG_ERRS_H */
---
+++ lib/errs.c
@@ -0,0 +1,193 @@
+/**
+ * Error stack.
+ *
+ * Copyright (C) 2016 Red Hat
+ *
+ * This file is part of tlog.
+ *
+ * Tlog is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tlog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tlog; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include <tlog/errs.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+const struct tlog_errs tlog_errs_nomem = {
+    .next = NULL,
+    .msg = "Failed allocating memory for the error stack, messages discarded",
+};
+
+void
+tlog_errs_destroy(struct tlog_errs **perrs)
+{
+    assert(perrs != NULL);
+
+    if (*perrs != NULL && *perrs != &tlog_errs_nomem) {
+        tlog_errs_destroy(&(*perrs)->next);
+        free((*perrs)->msg);
+        free(*perrs);
+    }
+}
+
+/**
+ * Replace a stack with tlog_errs_nomem, indicating memory allocation error.
+ *
+ * @param perrs Location of the stack to replace.
+ */
+static void
+tlog_errs_replace_with_nomem(struct tlog_errs **perrs)
+{
+    tlog_errs_destroy(perrs);
+    /* We promise to keep it intact */
+    *perrs = (struct tlog_errs *)&tlog_errs_nomem;
+}
+
+void
+tlog_errs_push(struct tlog_errs **perrs, struct tlog_errs *errs)
+{
+    if (perrs == NULL) {
+        tlog_errs_destroy(&errs);
+    } else if (errs != NULL) {
+        struct tlog_errs *last_errs;
+
+        for (last_errs = errs;
+             last_errs->next != NULL;
+             last_errs = last_errs->next);
+
+        if (last_errs == &tlog_errs_nomem) {
+            tlog_errs_destroy(perrs);
+        } else {
+            last_errs->next = *perrs;
+        }
+
+        *perrs = errs;
+    }
+}
+
+void
+tlog_errs_pushs(struct tlog_errs **perrs, const char *msg)
+{
+    struct tlog_errs *new_errs;
+    char *msg_copy;
+
+    assert(msg != NULL);
+
+    if (perrs != NULL) {
+        msg_copy = strdup(msg);
+        if (msg_copy == NULL) {
+            tlog_errs_replace_with_nomem(perrs);
+            return;
+        }
+
+        new_errs = malloc(sizeof(*new_errs));
+        if (new_errs == NULL) {
+            free(msg_copy);
+            tlog_errs_replace_with_nomem(perrs);
+            return;
+        }
+
+        new_errs->next = *perrs;
+        new_errs->msg = msg_copy;
+
+        *perrs = new_errs;
+    }
+
+    return;
+}
+
+void
+tlog_errs_pushc(struct tlog_errs **perrs, tlog_grc grc)
+{
+    assert(tlog_grc_is_valid(grc));
+
+    if (perrs != NULL) {
+        tlog_errs_pushs(perrs, tlog_grc_strerror(grc));
+    }
+}
+
+void
+tlog_errs_pushvf(struct tlog_errs **perrs, const char *fmt, va_list ap)
+{
+    struct tlog_errs *new_errs;
+    char *msg = NULL;
+
+    assert(fmt != NULL);
+
+    if (perrs != NULL) {
+        if (vasprintf(&msg, fmt, ap) < 0) {
+            /* Assume the only error possible is ENOMEM */
+            tlog_errs_replace_with_nomem(perrs);
+            return;
+        }
+
+        new_errs = malloc(sizeof(*new_errs));
+        if (new_errs == NULL) {
+            free(msg);
+            tlog_errs_replace_with_nomem(perrs);
+            return;
+        }
+
+        new_errs->next = *perrs;
+        new_errs->msg = msg;
+
+        *perrs = new_errs;
+    }
+
+    return;
+}
+
+void
+tlog_errs_pushf(struct tlog_errs **perrs, const char *fmt, ...)
+{
+    va_list ap;
+
+    assert(fmt != NULL);
+
+    if (perrs != NULL) {
+        va_start(ap, fmt);
+        tlog_errs_pushvf(perrs, fmt, ap);
+        va_end(ap);
+    }
+}
+
+int
+tlog_errs_print(FILE *stream, const struct tlog_errs *errs)
+{
+    int len = 0;
+    int rc;
+
+    assert(stream != NULL);
+
+    if (errs != NULL) {
+        /* Print lower error */
+        rc = tlog_errs_print(stream, errs->next);
+        if (rc < 0) {
+            return rc;
+        }
+        len += rc;
+
+        /* Print this error */
+        rc = fprintf(stream, "%s\n", errs->msg);
+        if (rc < 0) {
+            return rc;
+        }
+        len += rc;
+    }
+
+    return len;
+}
