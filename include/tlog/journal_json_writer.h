bed450b1f3dd821ca843a1602afb6aeb27cbe363
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2017-07-13 17:48:59 +0300

    Mention session_id parameter in journal writer description

--- include/tlog/journal_json_writer.h
+++ include/tlog/journal_json_writer.h
@@ -44,10 +44,11 @@ extern const struct tlog_json_writer_type tlog_journal_json_writer_type;
 /**
  * Create an instance of journal writer.
  *
- * @param pwriter   Location for the created writer pointer, will be set to
- *                  NULL in case of error.
- * @param priority  The "priority" argument to pass to journal(3).
- * @param username  Name of the user being recorded.
+ * @param pwriter       Location for the created writer pointer, will be set to
+ *                      NULL in case of error.
+ * @param priority      The "priority" argument to pass to journal(3).
+ * @param username      Name of the user being recorded.
+ * @param session_id    Audit session ID of the recording.
  *
  * @return Global return code.
  */
